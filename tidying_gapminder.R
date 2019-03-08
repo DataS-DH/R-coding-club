4d284cc34781f5d3747c8e5e4b38509cb35c95fb
DataS-DH <katie.davidson@dhsc.gov.uk>
2019-03-08 13:53:35 +0000

    Updates

--- tidying_gapminder.R
+++ tidying_gapminder.R
@@ -1,4 +1,21 @@
 install.packages("gapminder")
 library(gapminder)
+
+install.packages("tidyverse")
+library(tidyverse)
+
 gm_data <- gapminder
 
+gapminder %>%
+  filter(year == 2015) ->
+  gapminder15
+
+gapminder %>%
+  filter(year == 2007) ->
+  gapminder7
+
+gapminder7 %>% 
+  ggplot(aes(income, life_exp))
+
+gapminder7 %>%
+  ggplot(aes(gdpPercap, lifeExp))
