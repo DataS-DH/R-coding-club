9eecb892fe76162c16687788f809e42fb969806d
DHSC-DataScience <katie.davidson@dhsc.gov.uk>
2019-07-12 13:05:45 +0100

    Uploading overview script

---
+++ Shiny/Session1.R
@@ -0,0 +1,11 @@
+#Shiny session 1
+
+#first make sure we have the shiny package installed
+install.packages("shiny")
+
+
+#Shiny reference and documentation
+https://shiny.rstudio.com/reference/shiny/0.14/
+
+#you want to leave the library(shiny) line within the UI script
+
